904e8b6b4ff5c3334ae50795b3a64e51216a7951
alfred <aaaalfred93@outlook.com>
2024-04-01 17:20:47 +0800

    Initial commit

---
+++ 01_基础/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        <h1>hello alfred</h1>
+    </div>
+</template>
+
+<script lang="ts">
+</script>
+
+<style>
+    .app {
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="person">
+        <h2>name: {{ name }}</h2>
+        <h2>age: {{ age }}</h2>
+        <button @click="showTel">查看联系方式</button>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+    </div>
+</template>
+
+<script lang="ts" setup name="Person-alfred">
+let name = 'Alfred'
+let age = 18
+let tel = '17318400111'
+function changeName() {
+    name = '黄小俊'
+}
+function changeAge() {
+    age += 1
+    console.log(age);
+}
+function showTel() {
+    alert(tel)
+}
+</script>
+
+<style scoped>
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+}
+</style>
